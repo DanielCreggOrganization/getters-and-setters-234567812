5a3fe6087ecf5bed19a1fd97a7b8b91663c9a85d
Katarzyna Krakowska <g00364903@gmit.ie>
2020-02-27 14:01:46 +0000

    t

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    
     // Main method
+    public static void main(String[] args) {
+       Person personObject = new Person();
+       personObject.setFirstName("Kasia");
+       System.out.println(personObject.getFirstName());
+     
+}
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,34 @@
-class Person {
+public class Person {
+
     // Instance Variables
-    
+    private String firstName;
+    private String middleNames;
+    private String userName;
+
     // Methods
-}
+
+    //getter method
+    public String getFirstName(){
+            return firstName;
+   }
+    public String getMiddleNames(){
+            return middleNames;
+   }
+    public String getUserName(){
+            return userName;
+   }
+    //setter method
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setMiddleNames(String middleNames){
+        this.middleNames = middleNames;
+    }
+    public void setUserName(String UserName){
+        this.userName = userName;
+    }
+
+    
+   
+    }
+
